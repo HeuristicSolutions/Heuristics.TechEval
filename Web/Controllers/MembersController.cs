0a5bbab59a7b4251e56aac65902027c209fc7240
matt-chap <eatsalldaylol@hotmail.com>
2023-02-03 06:04:33 -0800

    Task 3 Email Duplicate Prevention

--- Web/Controllers/MembersController.cs
+++ Web/Controllers/MembersController.cs
@@ -5,10 +5,10 @@ using System.Web.Mvc;
 using Heuristics.TechEval.Core;
 using Heuristics.TechEval.Core.Models;
 using Heuristics.TechEval.Web.Models;
+using Newtonsoft.Json;
 
 namespace Heuristics.TechEval.Web.Controllers
 {
-
     public class MembersController : Controller {
 
 		private readonly DataContext _context;
@@ -25,35 +25,54 @@ namespace Heuristics.TechEval.Web.Controllers
 
 		[HttpPost]
 		public ActionResult New(NewMember data) {
-            if (data.Id > 0)
+            try
             {
-                var currentMember = _context.Members.FirstOrDefault(x => x.Id == data.Id);
-                if (currentMember != null)
+                if (ModelState.IsValid)
                 {
-                    currentMember.Name = data.Name;
-                    currentMember.Email = data.Email;
-                    currentMember.LastUpdated = DateTime.UtcNow;
+                    if (data.Id > 0)
+                    {
+                        var currentMember = _context.Members.FirstOrDefault(x => x.Id == data.Id);
+
+                        if (currentMember != null)
+                        {
+                            if (_context.Members.Any(x => x.Email == data.Email && x.Id != data.Id))
+                            {
+                                return new HttpStatusCodeResult((int)HttpStatusCode.BadRequest, "Duplicate email.");
+                            }
+                            currentMember.Name = data.Name;
+                            currentMember.Email = data.Email;
+                            currentMember.LastUpdated = DateTime.UtcNow;
+                        }
+                    }
+                    else
+                    {
+                        if (_context.Members.Any(x => x.Email == data.Email))
+                        {
+                            return new HttpStatusCodeResult((int)HttpStatusCode.BadRequest, "Duplicate email.");
+                        }
+                        var newMember = new Member
+                        {
+                            Name = data.Name,
+                            Email = data.Email,
+                            LastUpdated = DateTime.UtcNow
+                        };
+                        _context.Members.Add(newMember);
+                    }
+
+                    _context.SaveChanges();
+
+                    return Json("");
                 }
+
+                // TODO: Add errors to the view
+                var errors = ModelState.Values.SelectMany(v => v.Errors.Select(x => x.ErrorMessage)).ToList();
+                return new HttpStatusCodeResult((int)HttpStatusCode.BadRequest, JsonConvert.SerializeObject(errors));
             }
-            else
+            catch
             {
-                if (_context.Members.Any(x => x.Email == data.Email))
-                {
-                    return new HttpStatusCodeResult((int)HttpStatusCode.BadRequest, "Duplicate email.");
-                }
-                var newMember = new Member
-                {
-                    Name = data.Name,
-                    Email = data.Email,
-                    LastUpdated = DateTime.UtcNow
-                };
-                _context.Members.Add(newMember);
+                return new HttpStatusCodeResult((int)HttpStatusCode.BadRequest, "Unexpected Error.");
             }
-
-			_context.SaveChanges();
-
-			return Json("");
-		}
+        }
 
         [HttpGet]
         public ActionResult Details(int Id)
--- Web/Models/NewMember.cs
+++ Web/Models/NewMember.cs
@@ -1,12 +1,17 @@
-﻿namespace Heuristics.TechEval.Web.Models
-{
+﻿using System.ComponentModel.DataAnnotations;
 
+namespace Heuristics.TechEval.Web.Models
+{
 	/// <summary>
 	/// DTO representing the creation of a new Member
 	/// </summary>
 	public class NewMember {
         public int Id { get; set; }
+		[Required]
+		[RegularExpression("^[a-zA-Z0-9\\s]*$", ErrorMessage = "Only alphanumeric characters allowed")]
         public string Name { get; set; }
-		public string Email { get; set; }
+        [Required]
+		[EmailAddress]
+        public string Email { get; set; }
 	}
 }
