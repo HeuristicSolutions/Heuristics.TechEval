e9c54a35c0f5752870f9472efc7da9965433a5e4
Andrew Reyling <areyling@gmail.com>
2023-09-19 05:33:24 -0400

    Task 1 - Implement an edit screen for a Member

--- Web/Controllers/MembersController.cs
+++ Web/Controllers/MembersController.cs
@@ -5,35 +5,77 @@ using Heuristics.TechEval.Web.Models;
 using Heuristics.TechEval.Core.Models;
 using Newtonsoft.Json;
 using System;
+using System.Net;
 
-namespace Heuristics.TechEval.Web.Controllers {
-
-	public class MembersController : Controller {
-
+namespace Heuristics.TechEval.Web.Controllers
+{
+	public class MembersController : Controller
+	{
 		private readonly DataContext _context;
 
-		public MembersController() {
+		public MembersController()
+		{
 			_context = new DataContext();
 		}
 
-		public ActionResult List() {
+		public ActionResult List()
+		{
 			var allMembers = _context.Members.ToList();
 
 			return View(allMembers);
 		}
 
-		[HttpPost]
-		public ActionResult New(NewMember data) {
-			var newMember = new Member {
-				Name = data.Name,
-				Email = data.Email,
-				LastUpdated = DateTime.Now
-			};
+		[HttpGet]
+		public ActionResult Details(int id)
+		{
+			if (id == 0) return PartialView("_Details", new EditMember()); // no id, adding a new new member
 
-			_context.Members.Add(newMember);
+			var member = _context.Members.FirstOrDefault(_ => _.Id == id);
+			if (member == null) throw new Exception(string.Format("Member id {0} not found", id));
+
+			return PartialView("_Details", new EditMember
+			{
+				Id = member.Id,
+				Name = member.Name,
+				Email = member.Email
+			});
+		}
+
+		[HttpPost]
+		public ActionResult Edit(EditMember data)
+		{
+			if (data == null) BadRequest("No data provided");
+
+			if (data.Id == default)
+			{
+				// new member
+				_context.Members.Add(new Member
+				{
+					Name = data.Name,
+					Email = data.Email,
+					LastUpdated = DateTime.Now
+				});
+			}
+			else
+			{
+				// updating an existing member
+				var existing = _context.Members.FirstOrDefault(_ => _.Id == data.Id);
+				if(existing == null) return BadRequest(string.Format("Member id {0} not found", data.Id));
+
+				existing.Name = data.Name;
+				existing.Email = data.Email;
+				existing.LastUpdated = DateTime.Now;
+			}
+
 			_context.SaveChanges();
 
-			return Json(JsonConvert.SerializeObject(newMember));
+			var allMembers = _context.Members.ToList();
+			return PartialView("_List", allMembers);
+		}
+
+		private static ActionResult BadRequest(string message)
+		{
+			return new HttpStatusCodeResult((int)HttpStatusCode.BadRequest, message);
 		}
 	}
 }
---
+++ Web/Models/EditMember.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Heuristics.TechEval.Web.Models
+{
+	/// <summary>
+	/// DTO representing the basic details of a Member for creating or editing.
+	/// </summary>
+	public class EditMember
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Email { get; set; }
+	}
+}
